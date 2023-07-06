73c774325c0e3441ce4e45a589f77b9ebf53a8ea
sairajzero <sairaj0630@gmail.com>
2023-07-07 04:45:10 +0530

    remove standard-operations files

--- docs/scripts/floGlobals.js
+++
@@ -1,7 +0,0 @@
-const floGlobals = {
-    blockchain: "FLO",
-    marketID: "FKAEdnPfjXLHSYwrXQu377ugN4tXU7VGdf",
-    currency: "BTC"
-};
-
-('object' === typeof module) ? module.exports = floGlobals : null;
