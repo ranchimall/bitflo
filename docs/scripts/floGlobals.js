af98050cd4fea22fe9c8c8f4786522a56ff9b2fd
sairajzero <sairaj0630@gmail.com>
2023-07-07 04:49:49 +0530

    relocate required script files into docs/scripts

---
+++ docs/scripts/floGlobals.js
@@ -0,0 +1,7 @@
+const floGlobals = {
+    blockchain: "FLO",
+    marketID: "FKAEdnPfjXLHSYwrXQu377ugN4tXU7VGdf",
+    currency: "BTC"
+};
+
+('object' === typeof module) ? module.exports = floGlobals : null;
